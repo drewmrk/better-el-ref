fe2834bd3c63939bf0da9e079dd755f57135e299
Drew Markel <drewmrk8@gmail.com>
2021-05-16 21:11:03 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # better-el-ref
+
 A better way to reference HTML elements in JavaScript.
+
+## Reasoning
+
+Consistently calling `document.getElementById('id')` is long and tedious and adds unnecessary length to code. Not to mention, the usage of `document.querySelector('el')` is notoriously slow and so by using the `better-el-ref` package, referencing HTML elements is easy and optimized. `better-el-ref` also provides a nice way to handle elements that do not exist or are empty which keeps code DRY, especially when using TypeScript.
+
+## Usage
+
+There are three parameters in the `getElement()` function that `better-el-ref` provides; `element`, `method`, and `name`. Both `method` and `name` are optional but `element` is required. `element` is the string of the element that is to be referenced. `method` is the method of reference of which there are four options available, `id`, `tag`, `name`, and `class`. If the `method` parameter is left blank, the function defaults to using `document.querySelector('element')` instead. `name` is just the name of the element and is used to make debugging easier when an error is thrown.
+
+```js
+import getElement from 'better-el-ref'
+
+getElement(element, method?, name?)
+```
---
+++ src/index.ts
@@ -0,0 +1,67 @@
+/**
+ * getElement(): A better way to reference HTML elements (better-el-ref)
+ *
+ * @remarks
+ * Setup three function parameters, `element`, `method`, and `name`. Create a
+ * mutable variable (`el`) which will be assigned a method of referencing the
+ * HTML element based on the value of `method`. If the variable `el` is `null`
+ * or `undefined`, throw a `TypeError`, else check if `el` is an `object`, if it
+ * is, check the length. If `el` is an object and the ength of `el` is less
+ * than or equal to `1`, throw a `TypeError`. If no errors have been thrown,
+ * simply return `el`.
+ *
+ * @param element - Element to be referenced
+ * @param method - The method of referencing the element (id|tag|name|class)
+ * @param name - Name of the element to be referenced
+ *
+ * @returns HTML element
+ *
+ * @example
+ * // index.html
+ * <p id="test-element">Test</p>
+ *
+ * // index.js
+ * import getElement from 'better-el-ref'
+ * getElement('test-element', 'id', 'Paragraph test element')
+ */
+const getElement = (
+  element: string,
+  method?: 'id' | 'tag' | 'name' | 'class',
+  name?: string
+) => {
+  let el:
+    | HTMLElement
+    | HTMLCollectionOf<Element>
+    | NodeListOf<HTMLElement>
+    | Element
+    | null
+    | undefined
+  switch (method) {
+    case 'id':
+      el = document.getElementById(element)
+      break
+    case 'tag':
+      el = document.getElementsByTagName(element)
+      break
+    case 'name':
+      el = document.getElementsByName(element)
+      break
+    case 'class':
+      el = document.getElementsByClassName(element)
+      break
+    default:
+      el = document.querySelector(element)
+  }
+  if (el === null || el === undefined) {
+    throw new TypeError(`${name || 'Element'} is ${el}`)
+  } else {
+    if (el instanceof Object) {
+      if (Object(el).length === 0) {
+        throw new TypeError(`${name || 'Element'} is empty`)
+      }
+    }
+    return el
+  }
+}
+
+export default getElement
